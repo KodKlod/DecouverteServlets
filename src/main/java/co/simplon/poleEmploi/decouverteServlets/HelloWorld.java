704dbbd769382e13f00fe035eba5e8b89c5a914d
KodKlod <codingklod@gmail.com>
2017-05-03 15:47:05 +0200

    jsp dynamique

--- src/main/java/co/simplon/poleEmploi/decouverteServlets/HelloWorld.java
+++ src/main/java/co/simplon/poleEmploi/decouverteServlets/HelloWorld.java
@@ -3,6 +3,7 @@ package co.simplon.poleEmploi.decouverteServlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,27 +16,17 @@ public class HelloWorld extends HttpServlet {
 
 	public void init() throws ServletException {
 		// Do required initialization
-		message = "Hello World";
+		message = "Hello World Classe HelloWorld";
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Set response content type
-		response.setContentType("text/html");
-
-		// Actual logic goes here.
 		String prenom = request.getParameter("prenom");
-		PrintWriter out = response.getWriter();
-		if (prenom == null) {
-			out.println("<h1>" + message + "</h1>");
-		}
- else {
-		out.println("<h1>" + "Salut " + prenom + "</h1>");
-		}
-	}
-
-	public void destroy() {
-		// do nothing.
+		request.setAttribute("nom", prenom);
+		RequestDispatcher dispatcher;
+		dispatcher = request.getRequestDispatcher("/WEB-INF/hello.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	// La signature de cette méthode est invariable
@@ -43,7 +34,6 @@ public class HelloWorld extends HttpServlet {
 			throws ServletException, IOException {
 		// Set response content type
 		response.setContentType("text/html");
-
 		// Actual logic goes here.
 		String prenom = request.getParameter("prenom");
 		PrintWriter out = response.getWriter();
@@ -52,6 +42,10 @@ public class HelloWorld extends HttpServlet {
 		} else {
 			out.println("<h1>" + "Salut " + prenom + "</h1>");
 		}
+	}
+
+	public void destroy() {
+		// do nothing.
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -0,0 +1,16 @@
+<html>
+
+	<head>
+	<title>La page Hello.jsp</title>
+	</head>
+
+	<body>
+	<p>Découverte des Servlet - Hello.jsp</p>
+	<H2>Hello</H2>
+	<!--L'EL ci-dessous est définie dans la classe HelloWorld
+	méthode doPost
+	qui appelle hello.jsp à la place de index.jsp  -->
+	${nom}
+	</body>
+
+</html>
